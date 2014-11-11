5c4063136a86dd215180b6d3b7f0710179114e0e
Brian Dunnette <brian@dunnette.us>
2014-11-10 18:52:49 -0600

    initial commit

---
+++ daawo.js
@@ -0,0 +1,47 @@
+Patients = new Mongo.Collection('patients')
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault("counter", 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get("counter");
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set("counter", Session.get("counter") + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+  
+  Accounts.onLogin(function (users) {
+    /* if there's only one user, add that user to admin group */
+    if (Meteor.users.find().count() === 1) {
+      Roles.addUsersToRoles(users.user._id, ['admin']);
+    }
+  });
+}
+
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
+Router.route('/', function () {
+  this.render('hello', {
+    data: function () { return {patients: Patients.find()} }
+  });
+});
+
+Router.route('admin', {
+    path:'/admin',
+    template: 'accountsAdmin'
+});
